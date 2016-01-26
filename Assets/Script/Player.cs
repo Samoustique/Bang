af578365d2bb68aac67aedf5bebb50021e09ef71
Samoustique <samuel.philipps@gmail.com>
2016-01-26 19:32:16 +0100

    [Bang] Premier commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 10f;
+	
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+ 
+	void Update()
+	{
+		if (GetComponent<NetworkView>().isMine)
+		{
+			InputMovement();
+		}
+		else
+		{
+			SyncedMovement();
+		}
+	}
+	 
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		GetComponent<Rigidbody>().position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+ 
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.Z))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * speed * Time.deltaTime);
+ 
+        if (Input.GetKey(KeyCode.S))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.forward * speed * Time.deltaTime);
+ 
+        if (Input.GetKey(KeyCode.D))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * speed * Time.deltaTime);
+ 
+        if (Input.GetKey(KeyCode.Q))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * speed * Time.deltaTime);
+    }
+	
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = GetComponent<Rigidbody>().position;
+			stream.Serialize(ref syncPosition);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+	 
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+	 
+			syncStartPosition = GetComponent<Rigidbody>().position;
+			syncEndPosition = syncPosition;
+		}
+	}
+}
