6ac9d9671810efbec34f9a9ab7522e2eb84144b4
Samoustique <samuel.philipps@gmail.com>
2016-01-28 23:25:39 +0100

    [Bang] Ajout du Système de D&D du tuto et d'une scène Menu (à lancer en prems)

---
+++ Assets/Script/Draggable.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+    public Transform placeHolderParent = null;
+
+    public GameObject placeHolder = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+
+        placeHolder = new GameObject();
+        placeHolder.transform.SetParent(this.transform.parent);
+        LayoutElement layoutElement = placeHolder.AddComponent<LayoutElement>();
+        layoutElement.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        layoutElement.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        layoutElement.flexibleWidth = 0;
+        layoutElement.flexibleHeight = 0;
+
+        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        parentToReturnTo = this.transform.parent;
+        placeHolderParent = parentToReturnTo;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+
+        if (placeHolder.transform.parent != placeHolderParent)
+            placeHolder.transform.SetParent(placeHolderParent);
+
+        int newSiblingIndex = placeHolderParent.childCount;
+
+        for(int i = 0; i < placeHolderParent.childCount ; ++i)
+        {
+            if(this.transform.position.x < placeHolderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                if (placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                    newSiblingIndex--;
+                break;
+            }
+        }
+
+        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeHolder);
+    }
+}
---
+++ Assets/Script/DropZone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if (draggable != null)
+            draggable.placeHolderParent = this.transform;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if (draggable != null && draggable.placeHolderParent == this.transform)
+            draggable.placeHolderParent = draggable.parentToReturnTo;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if (draggable != null)
+            draggable.parentToReturnTo = this.transform;
+    }
+}
